e01d58d91bd0296fd7b0ce8f0c662710e569888a
mozukraken <mozukraken@gmail.com>
2018-02-24 22:19:34 +0900

    ビューの作成

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-iniset('display_errors', 1);
+ini_set('display_errors', 1);
 
 define('DNS', 'mysql:host=localhost;dbname=toDo_app');
 
--- index.php
+++ index.php
@@ -3,3 +3,31 @@
 require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/functions.php');
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>todo apps</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="" id="container">
+      <h1>Todos</h1>
+      <form action="">
+        <input type="text" name="" value="" placeholder="what needs to be done?">
+      </form>
+      <ul>
+        <li>
+          <input type="checkbox" name="" value="" id="new_todo">
+          <span>Do something</span>
+          <div class="delete_todo">×</div>
+        </li>
+        <li>
+          <input type="checkbox" checked name="" value="" id="">
+          <span class="done">Do something again</span>
+          <div class="delete_todo">×</div>
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
